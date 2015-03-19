1f724260e40b81d847bfd95334280289c505f695
Kieren Evans <kevans@inviqa.com>
2015-03-19 17:06:48 +0000

    Update Gem dependencies, "chef generate cookbook papertrail-cookbook", fix specs.
    This generates a bunch of files recommended by latest chef practises..

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,29 +1,28 @@
 require 'spec_helper'
 
-describe 'papertrail-cookbook::default' do
-  it 'require rsyslog' do
-    expect(runner.converge('papertrail-cookbook::default')).to include_recipe 'rsyslog'
+describe 'papertrail::default' do
+  let(:chef_run) {
+    ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '10.04') do |node|
+      node.set['papertrail']['watch_files'] = {
+        'test/file/name.jpg' => 'test_file'
+      }
+    end.converge(described_recipe)
+  }
+
+  it 'should require rsyslog' do
+    expect(chef_run).to include_recipe 'rsyslog'
   end
 
-  it 'uses the basename of the filename as the suffix for state file name' do
-    chef_run = runner('test', {
-      papertrail: {
-        watch_files: {
-          'test/file/name.jpg' => 'test_file'
-        }
-      }
-    }).converge('papertrail-cookbook::default')
-
-    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/60-watch-files.conf', '$InputFileName test/file/name.jpg'
-    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/60-watch-files.conf', '$InputFileTag test_file'
-    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/60-watch-files.conf', '$InputFileStateFile state_file_name_test_file'
+  it 'should use the basename of the filename as the suffix for state file name' do
+    expect(chef_run).to render_file('/etc/rsyslog.d/60-watch-files.conf').with_content('$InputFileName test/file/name.jpg')
+    expect(chef_run).to render_file('/etc/rsyslog.d/60-watch-files.conf').with_content('$InputFileTag test_file')
+    expect(chef_run).to render_file('/etc/rsyslog.d/60-watch-files.conf').with_content('$InputFileStateFile state_file_name_test_file')
   end
 
   it 'uses attributes to generate configuration' do
-    chef_run = runner.converge('papertrail-cookbook::default')
-    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/65-papertrail.conf', '$ActionResumeRetryCount -1'
-    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/65-papertrail.conf', '$ActionQueueMaxDiskSpace 100M'
-    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/65-papertrail.conf', '$ActionQueueSize 100000'
-    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/65-papertrail.conf', '$ActionQueueFileName /var/log/rsyslog_queue_main'
+    expect(chef_run).to render_file('/etc/rsyslog.d/65-papertrail.conf').with_content('$ActionResumeRetryCount -1')
+    expect(chef_run).to render_file('/etc/rsyslog.d/65-papertrail.conf').with_content('$ActionQueueMaxDiskSpace 100M')
+    expect(chef_run).to render_file('/etc/rsyslog.d/65-papertrail.conf').with_content('$ActionQueueSize 100000')
+    expect(chef_run).to render_file('/etc/rsyslog.d/65-papertrail.conf').with_content('$ActionQueueFileName papertrailqueue')
   end
 end
